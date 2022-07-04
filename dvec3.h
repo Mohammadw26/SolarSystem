9a5ee469e1f253d00580735aed2fb7127e8d931b
Mohammadw26 <mohammadw996@gmail.com>
2022-07-04 16:23:30 +0300

    final push

--- dvec3.h
+++ dvec3.h
@@ -40,6 +40,11 @@ public:
 		return dvec3(x * k, y * k, z * k);
 	}
 
+	dvec3 operator-()
+	{
+		return dvec3(-x, -y, -z);
+	}
+
 	double length()
 	{
 		return sqrt(x * x + y * y + z * z);
@@ -53,6 +58,20 @@ public:
 		z *= k;
 	}
 
+	void rotate(double angle, const dvec3& axis)
+	{
+		// rotation matrix
+		mat4x4 M;
+		mat4x4_identity(M);
+		mat4x4_rotate(M, M, (float)axis.x, (float)axis.y, (float)axis.z, (float)angle);
+
+		// matrix-vector multiplication
+		vec4 v = { (float)x, (float)y, (float)z, 0 };
+		vec4 r;
+		mat4x4_mul_vec4(r, M, v);
+		*this = dvec3(r[0], r[1], r[2]);
+	}
+
 	double x, y, z;
 };
 
@@ -63,11 +82,32 @@ dvec3 normalize(const dvec3& other)
 	return result;
 }
 
-void lookAt(mat4x4 m, dvec3 e, dvec3 c, dvec3 u)
+dvec3 cross(const dvec3& a, const dvec3& b)
 {
-	// convert dvec3 to vec3
-	vec3 eye = { (float)e.x, (float)e.y, (float)e.z };
-	vec3 center = { (float)c.x, (float)c.y, (float)c.z };
-	vec3 up = { (float)u.x, (float)u.y, (float)u.z };
-	mat4x4_look_at(m, eye, center, up);
+	return dvec3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+}
+
+void lookAt(mat4x4 m, dvec3 eye, dvec3 center, dvec3 up)
+{
+	// adapted from mat4x4_look_at()
+
+	dvec3 b = normalize(eye - center);
+	dvec3 r = normalize(cross(up, b));
+	dvec3 u = cross(b, r);
+
+	mat4x4_identity(m);
+
+	m[0][0] = (float)r.x;
+	m[0][1] = (float)u.x;
+	m[0][2] = (float)b.x;
+
+	m[1][0] = (float)r.y;
+	m[1][1] = (float)u.y;
+	m[1][2] = (float)b.y;
+
+	m[2][0] = (float)r.z;
+	m[2][1] = (float)u.z;
+	m[2][2] = (float)b.z;
+
+	mat4x4_translate_in_place(m, -(float)eye.x, -(float)eye.y, -(float)eye.z);
 }
